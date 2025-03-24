affaccfea14842433f7b978e288b9becefda0322
이희수 <hslee9234@naver.com>
2025-03-24 13:00:33 +0900

    feat: fornt example files

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>🚀 Hello from Dockerized React App!</h1>
+    </div>
+  );
+}
+
+export default App;
